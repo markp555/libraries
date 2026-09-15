03da84e89792a779a32a0bd8f4f38f0f25a7beaa
Mark Privodnov <madmark555@gmail.com>
2026-09-15 11:14:23 +0700

    feat: added template for ordered_set tests

---
+++ ordered_set/ordered_set.cpp
@@ -0,0 +1,16 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace orderedset
+{
+	TEST_CLASS(orderedset)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+		}
+	};
+}
